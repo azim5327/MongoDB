67426348928cf462ec98ca99942fe6ec1c5df7e0
azim5327 <114977136+azim5327@users.noreply.github.com>
2023-01-19 14:03:47 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -8,4 +8,11 @@ class User(db.Model, UserMixin):
      username = db.Column(db.String(150), unique=True)
      password = db.Column(db.String(150), unique=True)
      date_created = db.Column(db.Date(timezone=True), default=func.now())
+     posts = db.relationships('Post', backref='user,' passive_deletes=True)
      
+class Post(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+     date_created = db.Column(db.Date(timezone=True), default=func.now())
+     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+
