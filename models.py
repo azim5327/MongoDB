bb0d4dc271470d8b307d8362d29014c0b8fe76bb
azim5327 <114977136+azim5327@users.noreply.github.com>
2023-01-31 14:51:07 +0800

    Phythonblogtutorial4

--- models.py
+++ models.py
@@ -9,10 +9,23 @@ class User(db.Model, UserMixin):
      password = db.Column(db.String(150), unique=True)
      date_created = db.Column(db.Date(timezone=True), default=func.now())
      posts = db.relationships('Post', backref='user,' passive_deletes=True)
-     
+     comments = db.relationships('Comment', backref='user,' passive_deletes=True) 
+
+
 class Post(db.Model):
     id = db.Column(db.Interger, primary_key=True)
     text = db.Column(db.Text, nullable=False)
      date_created = db.Column(db.Date(timezone=True), default=func.now())
-     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+     author = db.Column(db.Integer, db.ForeignKey(
+         'user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationships('Comment', backref='post,' passive_deletes=True) 
 
+
+class Comment(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+     date_created = db.Column(db.Date(timezone=True), default=func.now())
+     author = db.Column(db.Integer, db.ForeignKey(
+         'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+         'post.id', ondelete="CASCADE"), nullable=False)
