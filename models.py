58557c24c0c5b6a0ea086233ef55f2af07517e53
azim5327 <114977136+azim5327@users.noreply.github.com>
2023-02-16 14:46:35 +0800

    Phythonblogtutorial5

--- models.py
+++ models.py
@@ -9,7 +9,8 @@ class User(db.Model, UserMixin):
      password = db.Column(db.String(150), unique=True)
      date_created = db.Column(db.Date(timezone=True), default=func.now())
      posts = db.relationships('Post', backref='user,' passive_deletes=True)
-     comments = db.relationships('Comment', backref='user,' passive_deletes=True) 
+     comments = db.relationships('Comment', backref='user,' passive_deletes=True)
+     likes = db.relationships('Like', backref='user,' passive_deletes=True)   
 
 
 class Post(db.Model):
@@ -19,12 +20,21 @@ class Post(db.Model):
      author = db.Column(db.Integer, db.ForeignKey(
          'user.id', ondelete="CASCADE"), nullable=False)
     comments = db.relationships('Comment', backref='post,' passive_deletes=True) 
-
+    likes = db.relationships('Like', backref='post,' passive_deletes=True)   
 
 class Comment(db.Model):
     id = db.Column(db.Interger, primary_key=True)
     text = db.Column(db.String(200), nullable=False)
      date_created = db.Column(db.Date(timezone=True), default=func.now())
+     author = db.Column(db.Integer, db.ForeignKey(
+         'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+         'post.id', ondelete="CASCADE"), nullable=False)
+
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.Date(timezone=True), default=func.now())
      author = db.Column(db.Integer, db.ForeignKey(
          'user.id', ondelete="CASCADE"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey(
